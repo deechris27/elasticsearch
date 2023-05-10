3260ee1b7fcc798d4348be6a14352dfb5eac7faf
deepak <dee@gmail.com>
2023-05-09 20:03:30 -0800

    elastic search

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+import React from "react";
+import {
+  ErrorBoundary,
+  Facet,
+  SearchProvider,
+  SearchBox,
+  Results,
+  PagingInfo,
+  ResultsPerPage,
+  Paging,
+  WithSearch
+} from "@elastic/react-search-ui";
+import {
+  BooleanFacet,
+  Layout,
+  SingleLinksFacet,
+  SingleSelectFacet
+} from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import { SearchDriverOptions } from "@elastic/search-ui";
+
+const connector = new AppSearchAPIConnector({
+  searchKey: "search-wokxta15kv1ndur5ecpudhdw",
+  engineName: "national-parks-demo",
+  endpointBase: "https://b81720545b184a168818feab4f7823f6.ent-search.us-central1.gcp.cloud.es.io"
+});
+
+const config: SearchDriverOptions = {
+  alwaysSearchOnInitialLoad: true,
+  apiConnector: connector,
+  hasA11yNotifications: true,
+  searchQuery: {
+    result_fields: {
+      title: { raw: {} }
+    },
+    search_fields: {
+      title: {}
+    },
+    disjunctiveFacets: [""],
+    facets: {}
+  }
+};
+
+function App() {
+  //const {test, setTest} = useState();
+  return (
+    <SearchProvider config={config}>
+      <WithSearch
+        mapContextToProps={({ wasSearched }) => ({
+          wasSearched
+        })}
+      >
+        {({ wasSearched }) => {
+          return (
+            <div className="App">
+              <ErrorBoundary>
+                <Layout
+                  header={<SearchBox debounceLength={0} />}
+                  sideContent={<div></div>}
+                  bodyContent={
+                    <Results
+                      titleField="title"
+                      urlField="nps_link"
+                      thumbnailField="image_url"
+                      shouldTrackClickThrough
+                    />
+                  }
+                  bodyHeader={
+                    <React.Fragment>
+                      {wasSearched && <PagingInfo />}
+                      {wasSearched && <ResultsPerPage options={[10,5]}/>}
+                    </React.Fragment>
+                  }
+                  bodyFooter={<Paging />}
+                />
+              </ErrorBoundary>
+            </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
+  );
+}
+
+export default App;
